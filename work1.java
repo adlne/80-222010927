ef7b5aed6a642e8db51ea4c39481402f76905679
adlne <149572338+adlne@users.noreply.github.com>
2023-11-03 13:30:07 +0200

    Update work1.java

--- work1.java
+++ work1.java
@@ -1,17 +1,17 @@
-
-
-
-class work1{//how to create method for reusability
-    public static double simpleinterest(int num1,double num2,int num3){ 
-        return num1 * num2 * num3;
-
+class work1{//define class called work1
+    public static double simpleinterest(int num1,double num2,int num3){ // Create a public static method named simpleinterest that takes three parameters://an integer num1
+    // - a double num2
+    // - an integer num3
+        return num1 * num2 * num3;// Calculate and return the simple interest using the provided formular
     }
-    public static void main(String[]args){
+    public static void main(String[]args){// The main method, the entry point of the program
         int number1=100000;
-        double number2=0.05;
+        double number2=0.05;// Declare and initialize three variables to hold values for simple interest calculation// Principal amount
+        double number2 = 0.05; // Interest rate
+        int number3 = 5;      // Time period
         int number3=5;
-        double result=simpleinterest(number1,number2,number3);
-        System.out.println( result);
+        double result=simpleinterest(number1,number2,number3);// Call the simpleinterest method with the provided values and store the result in 'result'
+        System.out.println( result);//Print the result of the simple interest calculation
     
 }
 }
