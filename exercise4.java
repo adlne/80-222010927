04e093eda034545781e9420821186417ffc0af5b
adlne <149572338+adlne@users.noreply.github.com>
2023-11-03 14:39:39 +0200

    Add files via upload

---
+++ exercise4.java
@@ -0,0 +1,9 @@
+class even{//define class named even
+    public static void main(String args[]){// The main method, the entry point of the program
+
+        int number;//declaration of varable called number
+        for(number=2;number<=10;number=number+2){// Use a for loop to iterate from 2 to 10 with increments of 2
+            System.out.println(number); // Print the current value of "number"
+        }
+    }
+}
