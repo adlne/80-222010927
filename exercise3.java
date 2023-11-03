a8245ff37b91d53707943e6b88a911f587ad7358
adlne <149572338+adlne@users.noreply.github.com>
2023-11-03 14:31:27 +0200

    Add files via upload

---
+++ exercise3.java
@@ -0,0 +1,23 @@
+class switchcase{//create class named switchcase
+    public static void main(String[] args)// The main method, the entry point of the program
+    {
+    int day=7;//declaration of of variable with its value
+    switch (day){// Start a switch statement with the 'day' variable as the expression to be evaluated.
+    case 1:System.out.println("monday"); // Case 1: If 'day' is 1, execute the following code.
+    break;// Exit the switch statement.
+    case 2:System.out.println("tuesday");// Case 1: If 'day' is 1, execute the following code
+    break;//Exit the switch statement.
+    case 3:System.out.println("wednesday");// Case 1: If 'day' is 1, execute the following code
+    break;//Exit the switch statement.
+    case 4:System.out.println("thursday");// Case 1: If 'day' is 1, execute the following code
+    break;//Exit the switch statement.
+    case 5:System.out.println("friday");// Case 1: If 'day' is 1, execute the following code
+    break;//Exit the switch statement.
+    case 6:System.out.println("saturday");// Case 1: If 'day' is 1, execute the following code
+    break;//Exit the switch statement.
+    case 7:System.out.println("sunday");// Case 1: If 'day' is 1, execute the following code
+    break;//Exit the switch statement.
+    default:System.out.println("not included");// Default case: If 'day' does not match any of the above cases, execute this code.
+}
+}
+}
