0359346814a9935bbe4c3be894beadf8eb22bbbf
adlne <149572338+adlne@users.noreply.github.com>
2023-11-03 14:18:10 +0200

    Add files via upload

---
+++ exercise2.java
@@ -0,0 +1,19 @@
+class comparison{// define class named comparison
+    public static void main(String args[]){// The main method, the entry point of the program
+
+        int marks=80;// declaration of variable by with its value
+        if (marks>=80)//if condition to check the marks
+        {
+            System.out.println("extremely success");//out put when marks>=80
+        }
+        else if(marks>=70)//else if condition when if condition is not true
+        {
+              System.out.println(" success only");//out put when marks>=70
+            }
+            else//final condition when all conditions was not true
+            {
+                  System.out.println("fail");//the out put when all conditions became false
+            }
+        }}
+
+
