8a594bb4041c3863abf645f2d1bef290b4fda001
adlne <149572338+adlne@users.noreply.github.com>
2023-11-03 11:54:49 +0200

    Update work4.java

--- work4.java
+++ work4.java
@@ -1,13 +1,13 @@
-public class work4 {
-    public double interest(int p,double r,int t,int n){
-        return p* (Math.pow((1+r/1200),n*t));
+public class work4 {//create public class named work4
+    public double interest(int p,double r,int t,int n){//create amethod named interest tha has four parameters(p,r,t,n)
+        return p* (Math.pow((1+r/1200),n*t));//this method  calculates compound interest and returns the result
     }
 }
-class Main{
-public static void main(String[] args) {
-work4 myclass= new work4();
-double answer=myclass.interest(500000,18,3,12);
-System.out.println("The Result is:"+answer);
+class Main{//define aseparate class named Main
+public static void main(String[] args) {//define the main method which is the entry point of program
+work4 myclass= new work4();//create ana instance of work4 class
+double answer=myclass.interest(500000,18,3,12);//call the interest method on the myclass object and store the result in the answer variable
+System.out.println("The Result is:"+answer);//print the result
 }
 }
 
