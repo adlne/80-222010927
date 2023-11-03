e8269669a9d747ec5f1aa656ef79b8834ed9c2cf
adlne <149572338+adlne@users.noreply.github.com>
2023-11-03 14:56:34 +0200

    Add files via upload

---
+++ exercise5.java
@@ -0,0 +1,9 @@
+class even{//create class named even
+    public static void main(String args[]){{// The main method, the entry point of the program
+        int number=2;// This line declares an integer variable "number" and initializes it with the value 2.
+    while(number<=10){// This is a while loop. It continues as long as the "number" is less than or equal to 10.
+            System.out.println(number);// This line prints the current value of "number" to the console.
+            number=+2;//  this line has a typo and should be corrected to "number += 2;" to work as intended.
+        }
+    }
+}
