f00e662dea06677ac1c88a490003427bf5c17207
adlne <149572338+adlne@users.noreply.github.com>
2023-11-03 13:48:54 +0200

    Add files via upload

---
+++ exercise1.java
@@ -0,0 +1,11 @@
+class operator {
+    public static void main(String[] args){// The main method, the entry point of the program
+        int a=10; ////valible declarations by assigning their values
+        int b=15;
+        if(a>b)//if condition to compare the greater variable between a and b 
+        {
+        System.out.println("a is greater than");//out put when the condition is true
+        }else
+        System.out.println("b is greater than");//out put when the condition is not true
+        }
+    }
